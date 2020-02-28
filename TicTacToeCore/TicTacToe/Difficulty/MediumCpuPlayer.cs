00406cd208009eb83dcb5158ff283a240ef38be9
Jerzy <53574429+jeryrep@users.noreply.github.com>
2020-02-28 14:38:30 +0100

    Removed redundant method

--- TicTacToeCore/TicTacToe/Difficulty/MediumCpuPlayer.cs
+++ TicTacToeCore/TicTacToe/Difficulty/MediumCpuPlayer.cs
@@ -1,96 +1,60 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace TicTacToeCore.TicTacToe.Difficulty
 {
     internal class MediumCpuPlayer : IDifficultyCpuPlayer
     {
-        private static int CheckLosingPosition(IReadOnlyList<Piece> board)
+        private static int CheckWinningOrLosingPosition(IReadOnlyList<Piece> board, Piece player)
         {
-            if ((board[0] == Piece.Empty && board[1] == Piece.X && board[2] == Piece.X) ||
-                (board[0] == Piece.Empty && board[3] == Piece.X && board[6] == Piece.X) ||
-                (board[0] == Piece.Empty && board[4] == Piece.X && board[8] == Piece.X))
+            if ((board[0] == Piece.Empty && board[1] == player && board[2] == player) ||
+                (board[0] == Piece.Empty && board[3] == player && board[6] == player) ||
+                (board[0] == Piece.Empty && board[4] == player && board[8] == player))
                 return 0;
-            if ((board[0] == Piece.X && board[1] == Piece.Empty && board[2] == Piece.X) ||
-                (board[1] == Piece.Empty && board[4] == Piece.X && board[7] == Piece.X))
+            if ((board[0] == player && board[1] == Piece.Empty && board[2] == player) ||
+                (board[1] == Piece.Empty && board[4] == player && board[7] == player))
                 return 1;
-            if ((board[0] == Piece.X && board[1] == Piece.X && board[2] == Piece.Empty) ||
-                (board[2] == Piece.Empty && board[5] == Piece.X && board[8] == Piece.X) ||
-                (board[2] == Piece.Empty && board[4] == Piece.X && board[6] == Piece.X))
+            if ((board[0] == player && board[1] == player && board[2] == Piece.Empty) ||
+                (board[2] == Piece.Empty && board[5] == player && board[8] == player) ||
+                (board[2] == Piece.Empty && board[4] == player && board[6] == player))
                 return 2;
-            if ((board[3] == Piece.Empty && board[4] == Piece.X && board[5] == Piece.X) ||
-                (board[0] == Piece.X && board[3] == Piece.Empty && board[6] == Piece.X))
+            if ((board[3] == Piece.Empty && board[4] == player && board[5] == player) ||
+                (board[0] == player && board[3] == Piece.Empty && board[6] == player))
                 return 3;
-            if ((board[3] == Piece.X && board[4] == Piece.Empty && board[5] == Piece.X) ||
-                (board[1] == Piece.X && board[4] == Piece.Empty && board[7] == Piece.X) ||
-                (board[0] == Piece.X && board[4] == Piece.Empty && board[8] == Piece.X) ||
-                (board[2] == Piece.X && board[4] == Piece.Empty && board[6] == Piece.X))
+            if ((board[3] == player && board[4] == Piece.Empty && board[5] == player) ||
+                (board[1] == player && board[4] == Piece.Empty && board[7] == player) ||
+                (board[0] == player && board[4] == Piece.Empty && board[8] == player) ||
+                (board[2] == player && board[4] == Piece.Empty && board[6] == player))
                 return 4;
-            if ((board[3] == Piece.X && board[4] == Piece.X && board[5] == Piece.Empty) ||
-                (board[2] == Piece.X && board[5] == Piece.Empty && board[8] == Piece.X))
+            if ((board[3] == player && board[4] == player && board[5] == Piece.Empty) ||
+                (board[2] == player && board[5] == Piece.Empty && board[8] == player))
                 return 5;
-            if ((board[6] == Piece.Empty && board[7] == Piece.X && board[8] == Piece.X) ||
-                (board[2] == Piece.X && board[4] == Piece.X && board[6] == Piece.Empty) ||
-                (board[0] == Piece.X && board[3] == Piece.X && board[6] == Piece.Empty))
+            if ((board[6] == Piece.Empty && board[7] == player && board[8] == player) ||
+                (board[2] == player && board[4] == player && board[6] == Piece.Empty) ||
+                (board[0] == player && board[3] == player && board[6] == Piece.Empty))
                 return 6;
-            if ((board[6] == Piece.X && board[7] == Piece.Empty && board[8] == Piece.X) ||
-                (board[1] == Piece.X && board[4] == Piece.X && board[7] == Piece.Empty))
+            if ((board[6] == player && board[7] == Piece.Empty && board[8] == player) ||
+                (board[1] == player && board[4] == player && board[7] == Piece.Empty))
                 return 7;
-            if ((board[6] == Piece.X && board[7] == Piece.X && board[8] == Piece.Empty) ||
-                (board[2] == Piece.X && board[5] == Piece.X && board[8] == Piece.Empty) ||
-                (board[0] == Piece.X && board[4] == Piece.X && board[8] == Piece.Empty))
-                return 8;
-            return -1;
-        }
-
-        private static int CheckWinningPosition(IReadOnlyList<Piece> board)
-        {
-            if ((board[0] == Piece.Empty && board[1] == Piece.O && board[2] == Piece.O) ||
-                (board[0] == Piece.Empty && board[3] == Piece.O && board[6] == Piece.O) ||
-                (board[0] == Piece.Empty && board[4] == Piece.O && board[8] == Piece.O))
-                return 0;
-            if ((board[0] == Piece.O && board[1] == Piece.Empty && board[2] == Piece.O) ||
-                (board[1] == Piece.Empty && board[4] == Piece.O && board[7] == Piece.O))
-                return 1;
-            if ((board[0] == Piece.O && board[1] == Piece.O && board[2] == Piece.Empty) ||
-                (board[2] == Piece.Empty && board[5] == Piece.O && board[8] == Piece.O) ||
-                (board[2] == Piece.Empty && board[4] == Piece.O && board[6] == Piece.O))
-                return 2;
-            if ((board[3] == Piece.Empty && board[4] == Piece.O && board[5] == Piece.O) ||
-                (board[0] == Piece.O && board[3] == Piece.Empty && board[6] == Piece.O))
-                return 3;
-            if ((board[3] == Piece.O && board[4] == Piece.Empty && board[5] == Piece.O) ||
-                (board[1] == Piece.O && board[4] == Piece.Empty && board[7] == Piece.O) ||
-                (board[0] == Piece.O && board[4] == Piece.Empty && board[8] == Piece.O) ||
-                (board[2] == Piece.O && board[4] == Piece.Empty && board[6] == Piece.O))
-                return 4;
-            if ((board[3] == Piece.O && board[4] == Piece.O && board[5] == Piece.Empty) ||
-                (board[2] == Piece.O && board[5] == Piece.Empty && board[8] == Piece.O))
-                return 5;
-            if ((board[6] == Piece.Empty && board[7] == Piece.O && board[8] == Piece.O) ||
-                (board[2] == Piece.O && board[4] == Piece.O && board[6] == Piece.Empty) ||
-                (board[0] == Piece.O && board[3] == Piece.O && board[6] == Piece.Empty))
-                return 6;
-            if ((board[6] == Piece.O && board[7] == Piece.Empty && board[8] == Piece.O) ||
-                (board[1] == Piece.O && board[4] == Piece.O && board[7] == Piece.Empty))
-                return 7;
-            if ((board[6] == Piece.O && board[7] == Piece.O && board[8] == Piece.Empty) ||
-                (board[2] == Piece.O && board[5] == Piece.O && board[8] == Piece.Empty) ||
-                (board[0] == Piece.O && board[4] == Piece.O && board[8] == Piece.Empty))
+            if ((board[6] == player && board[7] == player && board[8] == Piece.Empty) ||
+                (board[2] == player && board[5] == player && board[8] == Piece.Empty) ||
+                (board[0] == player && board[4] == player && board[8] == Piece.Empty))
                 return 8;
             return -1;
         }
 
         private static int SuggestedCell(IReadOnlyList<Piece> board)
         {
-            var winningCell = CheckWinningPosition(board);
-            if (winningCell != -1)
-                return winningCell;
-            var losingCell = CheckLosingPosition(board);
-            if (losingCell != -1)
-                return losingCell;
-            return -1;
+            var nextCell = CheckWinningOrLosingPosition(board, Piece.O);
+            if (nextCell != -1)
+                return nextCell;
+            nextCell = CheckWinningOrLosingPosition(board, Piece.X);
+            return nextCell != -1 ? nextCell : -1;
         }
 
-        public int GetNextMove(Piece[] board) => SuggestedCell(board) != -1 ? SuggestedCell(board) : new EasyCpuPlayer().GetNextMove(board);
+        public int GetNextMove(Piece[] board)
+        {
+            var nextMove = SuggestedCell(board);
+            return nextMove != -1 ? nextMove : new EasyCpuPlayer().GetNextMove(board);
+        }
     }
 }
